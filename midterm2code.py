0c3f8e19c2ee922587b4a1552d302ff02c1d5af7
Michael Fan <michael.fan@berkeley.edu>
2017-03-19 16:53:40 -0700

    finished chain matrix multiply, will update with code later

--- midterm2code.py
+++ midterm2code.py
@@ -1,3 +1,21 @@
+#Chain matrix multiplication optimization
+
+def chain_matrix_multiply(ordered_tuples):
+	
+
+
+
+
+#Problem 2: find the smallest lexicographic substring of length k
+
+def prob2(s, k):
+	subproblems = [[0 for x in range len(s)] for y in range k]
+
+
+
+
+
+
 #Problenm 3: find the longest common increasing subsequence between 2 arrays#
 
 def find(b, c):
@@ -9,8 +27,8 @@ def find(b, c):
 		n+=1
 	return -1
 
-def rFind(b, c):
-	n = len(b) - 1
+def rFind(b, c, i):
+	n = i
 	while n >= 0:
 		if b[n] == c:
 			# print c
@@ -18,21 +36,23 @@ def rFind(b, c):
 		n-=1
 	return -1
 
-def prob(a, b):
+def prob3(a, b):
 	subproblems = [0 for x in range(len(a))]
-	b_backIndices = [0 for x in range(len(a))]
-	b_frontIndices = [0 for x in range(len(a))]
+	b_backIndices = [-1 for x in range(len(a))]
 	
 	n = len(a) - 1
 	while n >= 0:
-		b_backIndices[n] = rFind(b, a[n])
-		b_frontIndices[n] = find(b, a[n])
+		b_backIndices[n] = rFind(b, a[n], len(b) - 1)
+		# b_frontIndices[n] = find(b, a[n])
 		c = n
 		while c < len(a):
-			if a[n] < a[c] and b_frontIndices[n] < b_backIndices[c]:
-				subproblems[n] = max(subproblems[n], subproblems[c])
+			if a[n] < a[c]:
+				tempIndex = rFind(b, a[n], b_backIndices[c])
+				b_backIndices[n] = max(b_backIndices[n], tempIndex)
+				if tempIndex != -1:
+					subproblems[n] = max(subproblems[n], subproblems[c])
 			c+=1
-		if b_frontIndices[n] != -1:
+		if b_backIndices[n] != -1:
 			subproblems[n] = subproblems[n] + 1
 		n-=1
 
@@ -47,4 +67,4 @@ def prob(a, b):
 a = [1, 2, 4, 2, 1, 6, 8]
 b = [2, 1, 4, 2, 4, 4, 2, 6]
 
-print prob(a, b)
+print prob3(a, b)
